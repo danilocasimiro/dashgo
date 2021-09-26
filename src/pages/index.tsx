ad37f7031254973f9f7ccab7636b2b9d240341eb
Danilo Casimiro <dan_casimiro@hotmail.com>
2021-09-26 13:17:47 -0300

    Criado todas as telas do sistema

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,36 @@
-export default function Home() {
+import { Flex, Button, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
+
+export default function SignIn() {
   return (
-    <h1>Dashgo</h1>
+    <Flex 
+      h="100vh" 
+      w="100vw" 
+      align="center" 
+      justify="center"
+    >
+      <Flex 
+        as="form" 
+        w="100%" 
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input name="email" type="email" label="E-mail"/>
+
+          <Input name="password" type="email" label="Senha"/>
+          
+        </Stack>
+        <Button 
+          type="submit" 
+          mt="6" 
+          colorScheme="pink"
+          size="lg"> Entrar </Button>
+      </Flex>  
+    
+    </Flex>
   )
 }

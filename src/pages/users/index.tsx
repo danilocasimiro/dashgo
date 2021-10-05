670f4464469f4868f3d7cbd23de49240124900e4
Danilo Casimiro <dan_casimiro@hotmail.com>
2021-10-04 21:40:32 -0300

    Finalizado a parte de paginacao

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -4,28 +4,10 @@ import Link from 'next/link';
 import { Header }  from '../../components/Header'
 import { Pagination } from '../../components/Pagination'
 import { Sidebar } from '../../components/Sidebar'
-import { useQuery } from 'react-query';
+import { useUsers } from '../../services/hooks/useUsers';
 
 export default function UserList() {
-  const { data, error, isLoading} = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users')
-    const data = await response.json()
-    
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: "numeric"
-        })
-      }
-    })
-
-    return users;
-  })
+  const { data, error, isLoading, isFetching} = useUsers()
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -41,7 +23,10 @@ export default function UserList() {
 
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">Usuários </Heading>
+            <Heading size="lg" fontWeight="normal">Usuários 
+            
+              { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" /> }
+            </Heading>
             <Link href="/users/create" passHref>
               <Button 
                 as="a" 
@@ -104,7 +89,10 @@ export default function UserList() {
                 }) }
               </Tbody>
             </Table>
-            <Pagination/>
+            <Pagination
+            totalCountOfRegisters={200}
+            currentPage={5}
+            onPageChange={() => {}}/>
           </>
         )}
         </Box>
